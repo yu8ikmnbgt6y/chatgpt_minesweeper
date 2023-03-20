c82af697b5bb5877d528bf7b313b7cc64747e20e
yu8ikmnbgt6y <yu8ikmnbgt6y@gmail.com>
2023-03-21 08:03:00 +0900

    update StartScreen

--- src/interface.py
+++ src/interface.py
@@ -10,14 +10,16 @@ class Interface:
     def __init__(self):
         self.root = tk.Tk()
         self.root.geometry("300x300")
-        self.start_screen = StartScreen(root=self.root)
+        self.start_screen = StartScreen(root=self.root, initialize_game_callback=self.initialize_game)
         # self.game_screen = GameScreen(root=self.root)
         # self.game_statistics_window = GameStatisticsWindow(root=self.root)
         # self.menu = Menu(root=self.root)
         self.scoreboard = ScoreBoard()
 
     def initialize_game(self, difficulty: str):
-        self.game_screen = GameScreen(difficulty)
+        print("START_GAME")
+        print("Not Implemented")
+        #self.game_screen = GameScreen(difficulty)
 
     def start(self):
         self.start_screen.pack()
--- src/start_screen.py
+++ src/start_screen.py
@@ -1,8 +1,27 @@
 import tkinter as tk
+from tkinter import ttk
 
 class StartScreen(tk.Frame):
-    def __init__(self, root=None, **kwargs):
-        super().__init__(root, **kwargs)
-        
-        self.label = tk.Label(self, text="Welcome to the game!")
-        self.label.pack()
+    def __init__(self, root, initialize_game_callback):
+        super().__init__(root)
+        self.initialize_game_callback = initialize_game_callback
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.pack(expand=True)
+
+        self.title_label = tk.Label(self, text="Minesweeper", font=("Helvetica", 24))
+        self.title_label.pack(pady=(50, 20))
+
+        self.difficulty_var = tk.StringVar()
+        self.difficulty_menu = ttk.Combobox(self, textvariable=self.difficulty_var, state="readonly")
+        self.difficulty_menu["values"] = ("beginner", "intermediate", "advanced")
+        self.difficulty_menu.current(0)
+        self.difficulty_menu.pack(pady=10)
+
+        self.start_button = tk.Button(self, text="Start Game", command=self.start_game)
+        self.start_button.pack(pady=10)
+
+    def start_game(self):
+        difficulty = self.difficulty_var.get()
+        self.initialize_game_callback(difficulty)

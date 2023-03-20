d9197fd636277a9f1ce1443698d143bd5d366fe4
yu8ikmnbgt6y <yu8ikmnbgt6y@gmail.com>
2023-03-20 22:26:30 +0900

    update by prommpt. --- set arguments like get_statics(self, difficulty), get_high_scores(self, difficulty). save statistics and high_scores as json file whose name is "save_data.json". load when constructer was called when there is "save_data.json" on the program root dir. --- add try-except at save_data when save "save_data.json". raise NotImplementedError when difficulty which is not "beginner" or "intermediate" or "advanced" at every functions have argument which has argument of difficulty. make helper function which check input difficulty is available.

--- src/score_board.py
+++ src/score_board.py
@@ -1,3 +1,4 @@
+import json
 from typing import Dict, List, Tuple
 
 class ScoreBoard:
@@ -8,11 +9,18 @@ class ScoreBoard:
             "advanced": {"games_played": 0, "games_won": 0, "win_percentage": 0},
         }
         self.high_scores = {"beginner": [], "intermediate": [], "advanced": []}
+        self.load_data()
 
-    def get_statistics(self) -> Dict[str, Dict[str, int]]:
-        return self.statistics
+    def _check_difficulty(self, difficulty: str) -> None:
+        if difficulty not in ["beginner", "intermediate", "advanced"]:
+            raise NotImplementedError("Invalid difficulty")
+
+    def get_statistics(self, difficulty: str) -> Dict[str, int]:
+        self._check_difficulty(difficulty)
+        return self.statistics[difficulty]
 
     def update_statistics(self, difficulty: str, won: bool) -> None:
+        self._check_difficulty(difficulty)
         self.statistics[difficulty]["games_played"] += 1
         if won:
             self.statistics[difficulty]["games_won"] += 1
@@ -20,18 +28,30 @@ class ScoreBoard:
             self.statistics[difficulty]["games_won"] / self.statistics[difficulty]["games_played"] * 100
         )
 
-    def get_high_scores(self) -> Dict[str, List[Tuple[int, str]]]:
-        return self.high_scores
+    def get_high_scores(self, difficulty: str) -> List[Tuple[int, str]]:
+        self._check_difficulty(difficulty)
+        return self.high_scores[difficulty]
 
     def update_high_scores(self, difficulty: str, time: int, timestamp: str) -> None:
+        self._check_difficulty(difficulty)
         self.high_scores[difficulty].append((time, timestamp))
         self.high_scores[difficulty].sort(key=lambda x: x[0])
         if len(self.high_scores[difficulty]) > 10:
             self.high_scores[difficulty].pop()
 
     def save_data(self) -> None:
-        # Save statistics and high_scores data to a file on disk
+        data = {"statistics": self.statistics, "high_scores": self.high_scores}
+        try:
+            with open("save_data.json", "w") as f:
+                json.dump(data, f)
+        except Exception as e:
+            print(f"Error saving data: {e}")
 
     def load_data(self) -> None:
-        # Load statistics and high_scores data from a file on disk, if it exists
-        # If the file does not exist, initialize the data with default values
+        try:
+            with open("save_data.json", "r") as f:
+                data = json.load(f)
+                self.statistics = data["statistics"]
+                self.high_scores = data["high_scores"]
+        except FileNotFoundError:
+            pass

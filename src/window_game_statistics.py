f2d39285d95c9c3985eed1798fbda0e7edafc551
yu8ikmnbgt6y <yu8ikmnbgt6y@gmail.com>
2023-03-22 16:29:10 +0900

    modified using gpt4

--- src/window_game_statistics.py
+++ src/window_game_statistics.py
@@ -15,62 +15,44 @@ class GameStatisticsWindow:
         self._statistics_window = tk.Toplevel(self._root)
         self._statistics_window.geometry("1000x800")
         self._statistics_window.title("Game Statistics")
-        header_labels = ["Rank", "Time (seconds)", "Date"]
 
-        # SCORE TABLE BLOCK
-        current_column = 0
-        for level in DIFFICULTIES:
+        # DIFFICULTY LABELS
+        for i, level in enumerate(DIFFICULTIES):
             level_label = tk.Label(self._statistics_window, text=level.capitalize(), font=("Helvetica", 10, "bold"))
-            level_label.grid(row=0, column=current_column, padx=10, pady=5, columnspan=3)
+            level_label.grid(row=0, column=i, padx=10, pady=5)
 
-            for col, header_text in enumerate(header_labels):
-                header_label = tk.Label(self._statistics_window, text=header_text, font=("Helvetica", 10, "bold"))
-                header_label.grid(row=1, column=current_column + col, padx=10, pady=5)
+        # HIGH SCORE TABLES
+        for i, level in enumerate(DIFFICULTIES):
+            tree = ttk.Treeview(self._statistics_window, columns=("Rank", "Time", "Date"), show="headings")
+            tree.column("Rank", width=40)
+            tree.column("Time", width=80)
+            tree.column("Date", width=150)
+            tree.heading("Rank", text="Rank")
+            tree.heading("Time", text="Time (seconds)")
+            tree.heading("Date", text="Date")
+            tree.grid(row=1, column=i, padx=10, pady=5)
 
             scores: List[HighScore] = scoreboard.get_high_scores(difficulty=level)
-            max_row = 0
-            for row, high_score in enumerate(scores, start=2):
+            for high_score in scores:
                 rank = high_score.rank
                 clear_time = high_score.clear_time
                 clear_date = high_score.clear_date
+                tree.insert("", "end", values=(rank, clear_time, clear_date))
 
-                rank_label = tk.Label(self._statistics_window, text=rank)
-                rank_label.grid(row=row, column=current_column, padx=10, pady=5)
+        # WIN RATE INFORMATION
+        for i, level in enumerate(DIFFICULTIES):
+            frame = tk.Frame(self._statistics_window)
+            frame.grid(row=2, column=i, padx=10, pady=5)
 
-                time_label = tk.Label(self._statistics_window, text=clear_time)
-                time_label.grid(row=row, column=current_column + 1, padx=10, pady=5)
-
-                date_label = tk.Label(self._statistics_window, text=clear_date)
-                date_label.grid(row=row, column=current_column + 2, padx=10, pady=5)
-                if row > max_row:
-                    max_row = row
-
-            separator = ttk.Separator(self._statistics_window, orient='vertical')
-            separator.grid(row=1, column=current_column + 3, rowspan=11, padx=10, pady=5, sticky='ns')
-
-            current_column += 4  # Add space between levels
-
-        separator = ttk.Separator(self._statistics_window, orient="horizontal")
-        separator.grid(row=13, column=0, padx=10, pady=10, columnspan=14, sticky="ew")
-
-        # WIN_RATE BLOCK
-        win_rate_block_stats_row = 14
-        current_column = 0
-        for level in DIFFICULTIES:
             games_played = scoreboard.get_statistics(level)["game_played"]
             games_won = scoreboard.get_statistics(level)["game_won"]
             win_rate = (games_won / games_played) * 100 if games_played > 0 else 0
 
-            games_played_label = tk.Label(self._statistics_window, text=f"play: {games_played}")
-            games_played_label.grid(row=win_rate_block_stats_row, column=current_column, padx=10, pady=5, columnspan=3)
+            games_played_label = tk.Label(frame, text=f"play: {games_played}")
+            games_played_label.pack(side="top", padx=10, pady=5)
 
-            games_won_label = tk.Label(self._statistics_window, text=f"win: {games_won}")
-            games_won_label.grid(row=win_rate_block_stats_row + 1, column=current_column, padx=10, pady=5, columnspan=3)
+            games_won_label = tk.Label(frame, text=f"win: {games_won}")
+            games_won_label.pack(side="top", padx=10, pady=5)
 
-            win_rate_label = tk.Label(self._statistics_window, text=f"win_rate: {win_rate:.2f}%")
-            win_rate_label.grid(row=win_rate_block_stats_row + 2, column=current_column, padx=10, pady=5, columnspan=3)
-
-            separator = ttk.Separator(self._statistics_window, orient='vertical')
-            separator.grid(row=win_rate_block_stats_row, column=current_column + 3, rowspan=3, padx=10, pady=5, sticky='ns')
-
-            current_column += 4  # Add space between levels
+            win_rate_label = tk.Label(frame, text=f"win_rate: {win_rate:.2f}%")
+            win_rate_label.pack(side="top", padx=10, pady=5)
